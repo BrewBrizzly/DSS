1e77172fc617c12869753b2944bb8bfceb4c910e
BrewBrizzly <stephannl12@hotmail.com>
2021-11-09 16:39:07 +0100

    patch ready for peregrine

--- Pre_Processing/Parchment_Patches/arg.py
+++ Pre_Processing/Parchment_Patches/arg.py
@@ -11,21 +11,37 @@ from patches import *
 
 def read(directory):
 
-	# Keeping track of all patches created 
-	patch_number = 0 
+    # Looping through each plate in dir
+    for plate_number in os.listdir(directory):
 
-	# Looping through each file in a directory 
-	for filename in os.listdir(directory):
+        # Keeping track of all patches created per fragment
+        n = 0 
 
-		# If file is jpg
-		if filename.endswith('.jpg'):
+        # Joining the path 
+        d = os.path.join(directory, plate_number)
 
-			# loading the fragment and mask as cv2 struct with flag 1 color and flag 0 gray-scale
-			fragment = load(directory + filename, 1)
+        # If d is a dir to a plate 
+        if os.path.isdir(d):
 
-			# Padding the fragment to fit n by n patch dimension 
-			fragment = padding(fragment, 256)
+            # Looping to each fragment belonging to a plate 
+            for filename in os.listdir(plate_number):
 
-			# Extracting and saving n by n patches from the fragment 
-			patch_number = patches(fragment, directory, patch_number, 256)
+                # If file is jpg
+                if filename.endswith('.jpg'):
+
+                    # Creating the file path 
+                    file_path = d + '/' + filename
+
+                    # Creating the dir name for the patches to be stored 
+                    dir_name_patches = filename.split('.')
+                    dir_name_patches = dir_name_patches[0]
+
+                    # loading the fragment and mask as cv2 struct with flag 1 color and flag 0 gray-scale
+                    fragment = load(file_path, 1)
+
+                    # Padding the fragment to fit n by n patch dimension 
+                    fragment = padding(fragment, 256)
+
+                    # Extracting and saving n by n patches from the fragment 
+                    patch_number = patches(fragment, 'data/p301438/IAApatches_parchment_onlyColor-resized50/' + plate_number + '/' + dir_name_patches + '/', n, 256)
 
--- Pre_Processing/Parchment_Patches/patches.py
+++ Pre_Processing/Parchment_Patches/patches.py
@@ -8,19 +8,19 @@ from skimage.util import view_as_blocks
 
 def patches(fragment, directory, patch_number, n):
 
-	# Extracting the patches from the fragment 
-	patches = view_as_blocks(fragment, (256, 256, 3))
+    # Extracting the patches from the fragment 
+    patches = view_as_blocks(fragment, (256, 256, 3))
 
-	# Saving each patch by going through the collumns and rows of patches
-	for col in patches:
-		for row in col:
+    # Saving each patch by going through the collumns and rows of patches
+    for col in patches:
+        for row in col:
                         
-                        # Reducing dimensionality 
-                        patch = row[0]
+            # Reducing dimensionality 
+            patch = row[0]
 
-                        # If patch does not contain only mask, then save
-                        if np.any(patch):
-                                cv2.imwrite(directory + 'Test_Patches/patch_' +  str(patch_number) + '.jpg', patch)
-                                patch_number += 1
-                        
-	return patch_number
+            # If patch does not contain only mask, then save
+            if np.any(patch):
+                    cv2.imwrite(directory + 'patch_' +  str(patch_number) + '.jpg', patch)
+                    patch_number += 1
+            
+    return patch_number
