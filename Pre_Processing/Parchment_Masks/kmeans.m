fa3b18e706ff61425deec476f20d277f35325ae1
BrewBrizzly <stephannl12@hotmail.com>
2021-11-16 15:42:07 +0100

    Everything ready for Peregrine

--- Pre_Processing/Parchment_Masks/kmeans.m
+++ Pre_Processing/Parchment_Masks/kmeans.m
@@ -1,4 +1,4 @@
-% Possible folders depending on system
+% Possible parameters depending on system
 
 % myFolder = '/data/p301438/IAAfragments_onlyColor-resized50';
 % myFolder = '/projects/mdhali/BscProjects/Stephan/IAAfragments_parchment_onlyColor-resized50/';
@@ -24,13 +24,20 @@
 % Dir_mask = ['/Users/stephannijdam/Desktop/test/mask/', newDir];
 % Dir_mask = ['C:\Users\Stephan\Desktop\BachelorP\DSS\Pre_Processing\Parchment_Masks\mask\', newDir];
 
+% Possible dir for Binarized image
+
+% Binarized_path =  ['/data/p301438/IAAfragments_onlyBinarized/', newDir ,'/', extractBefore(baseFileName, '-D'), '.jpg'];
+
+
+% Below the actual code 
+
 % Specify the folder where the files live.
 
-myFolder = 'C:\Users\Stephan\Desktop\BachelorP\DSS\Pre_Processing\Parchment_Masks';
+myFolder = '/data/p301438/IAAfragments_onlyColor-resized50';
 
 % File pattern, in this case .jpg
 
-filePattern = fullfile(myFolder, '/*.jpg');
+filePattern = fullfile(myFolder, '**/*.jpg');
 
 % List of all the names of the files that match pattern
 
@@ -38,29 +45,38 @@ theFiles = dir(filePattern);
 
 for k = 1 : length(theFiles)
 
-    % Determine the path to the file 
+    % Determine the path to the fragment
 
     baseFileName = theFiles(k).name;
     fullFileName = fullfile(theFiles(k).folder, baseFileName);
+
+    % Getting the dir to store the mask, fragment and the path to the
+    % binarized image
+
+    position = find(theFiles(k).folder == '/', 1, 'last');
+    newDir = extractAfter(theFiles(k).folder, position);
     
+    % Dir for extracted frag  
+
+    Dir_frag = ['/data/p301438/IAAfragments_isolated_parchment_onlyColor-resized50/', newDir];
+
+    % Dir for extracted mask 
+
+    Dir_mask = ['/data/p301438/IAAfragments_parchment_mask_onlyColor-resized50/', newDir];
+    
+    % Path to binarized image 
+
+    Binarized_path =  ['/data/p301438/IAAfragments_onlyBinarized/', newDir ,'/', extractBefore(baseFileName, '-D'), '.jpg'];
+
     % Reading the fragment
 
     I = imread(fullFileName);
 
-    % Performing image segmentation via k-means of 2 
+    % Converting grayscale fragment image to binarized
 
-    [L,Centers] = imsegkmeans(I, 2);
+    BW1 = im2bw(I, 0.25);
     
-    % Converting every value 2 to 255im
-
-    L(L==2)=255;
-    
-    % Converting grayscale image to binarized
-
-    BW1 = imbinarize(L, 0.5);
-    %BW1 = im2bw(I, 0.5);
-    
-    % Removing small white pixels 
+    % Removing small white pixels before image-infilling 
 
     BW2 = bwareaopen(BW1, 50);
     
@@ -68,11 +84,14 @@ for k = 1 : length(theFiles)
 
     BW3 = imfill(BW2, 'holes');
 
+    % Removing larger white left-overs after image-infilling 
+    BW4 = bwareaopen(BW1, 600);
+
     % Code lines 60 to 83 for grabbing the object closest to the center of the image
     % obtained from https://nl.mathworks.com/matlabcentral/answers/797442-find-the-object-closest-to-the-center
 
-    [rows, columns] = size(BW3);
-    [labeledImage, numBlobs] = bwlabel(BW3);
+    [rows, columns] = size(BW4);
+    [labeledImage, numBlobs] = bwlabel(BW4);
 
     % Measuring the centroid of each object 
 
@@ -92,21 +111,30 @@ for k = 1 : length(theFiles)
     % Creating the mask of the object closest to center 
 
     Mask = ismember(labeledImage, sortOrder(1:1));
+
+    % Reading corresponding binarized image, if it exists to extend the
+    % mask
+    if isfile(Binarized_path)
+
+        Binarized_image = imread(Binarized_path);
+    
+        % Converting the binarized image to binary format 
+        B2 = im2bw(Binarized_image, 0.5);
+        
+        % Taking the inverse of the binarized image since we want to 
+        % add the text to the mask 
+        B3 = 1 - B2;
+        
+        % Combining the orignal Mask with the binarized image 
+        Mask = bitor(Mask, B3);
+
+    end
     
     % Combining the mask and fragment
 
     Res = bsxfun(@times, I, cast(Mask, 'like', I));
 
-    % Getting the dir to store the mask and fragment 
-
-    position = find(theFiles(k).folder == '\', 1, 'last');
-    newDir = extractAfter(theFiles(k).folder, position);
-    
-    % Dir for extracted frag  
-
-    Dir_frag = ['C:\Users\Stephan\Desktop\BachelorP\DSS\Pre_Processing\Parchment_Masks\frag\', newDir];
-
-    % Create dir if not exist 
+    % Create dir for frag if not exist 
 
     if ~exist(Dir_frag, 'dir')
        mkdir(Dir_frag)
@@ -114,13 +142,9 @@ for k = 1 : length(theFiles)
 
     % Saving the 50 percent downscaled combination of fragment and mask 
 
-    imwrite(Res, [Dir_frag,'\', baseFileName]);
-    
-    % Dir for extracted mask 
+    imwrite(Res, [Dir_frag,'/', baseFileName]);
 
-    Dir_mask = ['C:\Users\Stephan\Desktop\BachelorP\DSS\Pre_Processing\Parchment_Masks\mask\', newDir];
-
-     % Create dir if not exist 
+     % Create dir for mask if not exist 
 
     if ~exist(Dir_mask, 'dir')
        mkdir(Dir_mask)
@@ -128,6 +152,6 @@ for k = 1 : length(theFiles)
     
     % Saving the mask
     
-    imwrite(Mask, [Dir_mask,'\', baseFileName]);
+    imwrite(Mask, [Dir_mask,'/', baseFileName]);
 
 end
