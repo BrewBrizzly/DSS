2896297607041c3cc2ceece61dac9874ada2f04f
BrewBrizzly <stephannl12@hotmail.com>
2021-11-17 16:05:30 +0100

    changed patch dir

--- Pre_Processing/Parchment_Patches/main.py
+++ Pre_Processing/Parchment_Patches/main.py
@@ -8,7 +8,7 @@ from arg import *
 def main():
 
 	# Reading the paths to fragment and performing the pre-processing steps 
-	read('/data/p301438/IAAfragments_parchment_onlyColor-resized50')
+	read('/data/p301438/IAAfragments_isolated_parchment_onlyColor-resized50')
 
 if __name__ == '__main__':
 	main()
