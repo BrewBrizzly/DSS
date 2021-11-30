2539d3daf52618bd58e0ffd8ade457553009e0a9
BrewBrizzly <stephannl12@hotmail.com>
2021-11-30 12:47:49 +0100

    GPU issues

--- Siamese_VGG16/model.py
+++ Siamese_VGG16/model.py
@@ -8,20 +8,27 @@ from tensorflow.keras.layers import Input
 from tensorflow.keras.layers import Lambda
 from tensorflow.keras.datasets import mnist
 
+# Just disables the warning, doesn't take advantage of AVX/FMA to run faster
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
 # Function that connects two CNNS via a ECL layer
 # which is connected to a block of dense layers 
-def build_Siamese_network(inputShape = 256):
+def build_Siamese_network(inputShape):
 
-	# Configuring the inputshape of the model 
-	imgTop = Input(shape = inputShape)
-	imgBottom = Input(shape = inputShape)
+	# Configuring input layers for each sister
+	input1 = Input(shape = inputShape)
+	input2 = Input(shape = inputShape)
 
-	# Functions returns a VGG16 model
-	CNNTop = build_VGG16_network(imgTop)
-	CNNBottom = build_VGG16_network(imgBottom) 
+	# Configuring a VGG16 model based on the input shape 
+	VGG16 = build_VGG16_network(inputShape)
 
-	# Connecting the Sister networks to lambda euclidian distance layer 
-	distance = Lambda(euclidean_distance)([CNNTop, CNNBottom])
+	# Building the two sisters layers by connecting the input layers and the VGG16
+	sister1 = VGG16(input1)
+	sister2 = VGG16(input2)
+
+	# Connecting the Sister layers to lambda euclidian distance layer 
+	distance = Lambda(euclidean_distance)([sister1, sister2])
 
 	# Creating the final dense layers 
 	x = Dense(512, activation="relu", name = 'block6_dense1')(distance)
@@ -31,7 +38,9 @@ def build_Siamese_network(inputShape = 256):
 	# building the complete model 
 	model = Model(inputs=[CNNTop, CNNBottom], outputs=outputs)	
 
+	print("test")
+
 	# Summary of the complete model 
 	model.Summary()
 
-build_VGG16_network()
+build_VGG16_network((256, 256, 3))
