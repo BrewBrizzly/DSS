beb9c948ec8df918e1084f88dd3743b59d31f820
BrewBrizzly <stephannl12@hotmail.com>
2021-12-07 18:35:38 +0100

    testing

---
+++ Pre_Processing/Test_env/main.py
@@ -0,0 +1,12 @@
+# Scripts for creating lists of paths for each cutoff 
+# Followingly these paths are passed to create plots 
+# and a summary
+
+
+def main():
+	
+	path = 'Path'
+	create_paths(path)	
+
+if __name__ == '__main__':
+	main()
