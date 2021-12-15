410afde7e9123d2ca3796ffe9e0c617863dc0f74
S.H. Nijdam <s3690970@ai17871.ai.rug.nl>
2021-12-15 11:16:19 +0100

    bin created

--- Data_Processing /Extract_Bins/extract_bins.py
+++ Data_Processing /Extract_Bins/extract_bins.py
@@ -1,7 +1,8 @@
-# Creating a bin structure in the lists
+# Creating a bin structure in the arrays
 
 # Requiered libraries
 import numpy as np 
+import os
 
 # Looping through all the arrays
 def loop_arrays(path):
@@ -13,13 +14,16 @@ def loop_arrays(path):
 		if arr_file.endswith('.npy'):
 
 			# Creating the path to an array
-			arr_path = os.dir.join(path, arr_file)
+			arr_path = os.path.join(path, arr_file)
 
 			# Loading the array
 			arr = np.load(arr_path, allow_pickle = True)
 
 			# Creating a sorted bins list for an array
 			bins_ls = create_bins_ls(arr)
+
+			# Confirmation
+			print("Bins: ", bins_ls)
 
 			# Creating the bin structure in the array
 			create_bins_structure(arr, bins_ls, arr_file)
@@ -29,25 +33,25 @@ def loop_arrays(path):
 def create_bins_ls(arr):
 
 	# Creating a list containing an oredered bins of array
-	bin_ls = []
+	bins_ls = []
 
 	# Looping through all instances to get the bin values
 	for instance in arr:
 
 		# If bin value is not in the list 
-		if len(instance) not in bin_ls:
+		if len(instance) not in bins_ls:
 
-			bin_ls.append(instance)
+			bins_ls.append(len(instance))
 
 	# Order the bin list 
-	bin_ls.sort()
+	bins_ls.sort()
 
 	# Returning the sorted list 
 	return bins_ls
 
-
 # Creating the bin structure in an array
-def create_bins_structure(arr, bins, name)
+# bin_strc_i[tmp_bin_i[fragment_i[]]]
+def create_bins_structure(arr, bins, name):
 
 	# List for bin structure
 	bin_strc = []
@@ -62,14 +66,17 @@ def create_bins_structure(arr, bins, name)
 		for instance in arr:
 
 			# If a instane of paths belongs to a bin 
-			if len(instance) == bn[i]:
+			if len(instance) == bins[i]:
 
 				tmp_bin.append(instance)
 
 		bin_strc.append(tmp_bin)
 
 	# Storing the bin_struct 
-	np.save('Lists_sorted/' + name, bin_strc, allow_pickle = True)
+	np.save('/projects/mdhali/BscProjects/Stephan/Sorted_paths/Arrays/' + name, bin_strc, allow_pickle = True)
+
+	# Confirmation
+	print("Saved: " + name)
 
 
 
--- Data_Processing /Extract_Bins/main.py
+++ Data_Processing /Extract_Bins/main.py
@@ -5,7 +5,11 @@ from extract_bins import *
 
 
 def main():
-    path = '/projects/mdhali/BscProjects/Stephan/Unsorted_paths/Cutoff_15/Arrays/'
+
+    # Path to the arrays we want to create a bin structure in 
+    path = '/projects/mdhali/BscProjects/Stephan/Unsorted_paths/Arrays/'
+
+    # Creating the bin structure for the array
     loop_arrays(path)
 
 if __name__ == '__main__':
