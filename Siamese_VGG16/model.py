bde2915dd08cf248c8b1c52dba0a391f812de324
BrewBrizzly <stephannl12@hotmail.com>
2022-01-04 15:30:34 +0100

    updated

--- Siamese_VGG16/model.py
+++ Siamese_VGG16/model.py
@@ -6,24 +6,24 @@ import os
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras.metrics import Precision, Recall
+from tensorflow.keras.metrics import Precision, Recall, Accuracy
 
-# Function that connects two CNNS via a ECL layer
-# which is connected to a block of dense layers 
+# Function that connects two CNNS via a eucledian distance layer
+# which is connected to a block of dense layers, the output 
 def build_Siamese_network():
 
-    # Configuring input layers for each sister
+    # Configuring the two input layers 
     Input_Image_A = Input(name = 'Image_A', shape = (256, 256, 3))
     Input_Image_B = Input(name = 'Image_B', shape = (256, 256, 3))
 
-    # Defining the VGG16
+    # Defining the VGG16 
     VGG16 = build_VGG16_network()
 
-    # Connecting each image to VGG16
+    # Connecting input layer to a VGG16
     VGG16_A = VGG16(Input_Image_A)
     VGG16_B = VGG16(Input_Image_B)
 
-    # Connecting the each layer to lambda euclidian distance layer 
+    # Connecting each VGG16 to the lambda euclidian distance layer 
     distance = keras.layers.Lambda(euclidean_distance)([VGG16_A, VGG16_B])
 
     # Creating the final dense layers 
@@ -37,7 +37,7 @@ def build_Siamese_network():
     return model
 
 
-# Normalizing pixel values, no need for scaling 
+# Normalizing pixel values of the data, no need for scaling 
 def preprocess_img(path):
 
     # Read the image
@@ -59,11 +59,11 @@ def process_pairs(a, b, label):
 # Building the left and right dataset
 def build_data(Path_Positive_A, Path_Positive_B, Path_Negative_A, Path_Negative_B):
 
-    # Getting the filenames for the positive pair, in same order 
+    # Getting the filenames for the positive pair, in same order by setting the seed 
     P_a = tf.data.Dataset.list_files(Path_Positive_A, shuffle = True, seed = 24)
     P_b = tf.data.Dataset.list_files(Path_Positive_B, shuffle = True, seed = 24)
 
-    # Getting the directories for the negative pair, in same order 
+    # Getting the filenames for the negative pair, in same order by setting the seed 
     N_a = tf.data.Dataset.list_files(Path_Negative_A, shuffle = True, seed = 24)
     N_b = tf.data.Dataset.list_files(Path_Negative_B, shuffle = True, seed = 24)
 
@@ -79,13 +79,10 @@ def build_data(Path_Positive_A, Path_Positive_B, Path_Negative_A, Path_Negative_
     # Processing the dataset
     dataset = dataset.map(process_pairs)
 
-    # Caching for performance 
-    dataset = dataset.cache()
-
     # Shuffling for performance in training
-    dataset = dataset.shuffle(buffer_size = 1024)
+    dataset = dataset.shuffle(buffer_size = 2000)
 
-    # Batching the dataset for prefenting bottleneck 
+    # Batching the dataset for prefenting bottleneck, size according paper
     dataset = dataset.batch(16) 
 
     # Prefetching for performance 
@@ -95,87 +92,107 @@ def build_data(Path_Positive_A, Path_Positive_B, Path_Negative_A, Path_Negative_
 
 
 # From https://github.com/nicknochnack/FaceRecognition/blob/main/Facial%20Verification%20with%20a%20Siamese%20Network%20-%20Final.ipynb
+# Performs one training step of the model during an epoch 
 @tf.function
 def train_step(siamese_model, batch, binary_cross_loss, opt):
     
     # Record all of our operations 
-    with tf.GradientTape() as tape:     
-        # Get anchor and positive/negative image
+    with tf.GradientTape() as tape:    
+
+        # Getting one pair out of the batch, current implementation might be wrong 
         X = batch[:2]
-        # Get label
+
+        # Getting the label of the pair 
         y = batch[2]
         
-        # Forward pass
+        # A single forward pass with a pair 
         yhat = siamese_model(X, training=True)
-        # Calculate loss
-        loss = binary_cross_loss(y, yhat)
 
-    print(loss)
+        # Calculate loss with the label and the predicted outcome 
+        loss = binary_cross_loss(y, yhat)
         
-    # Calculate gradients
+    # Calculate the gradients that need to be applied to the model 
     grad = tape.gradient(loss, siamese_model.trainable_variables)
     
-    # Calculate updated weights and apply to siamese model
+    # Applying the gradients to the model 
     opt.apply_gradients(zip(grad, siamese_model.trainable_variables))
         
-    # Return loss
+    # Return the current loss value
     return loss
 
 
 # From https://github.com/nicknochnack/FaceRecognition/blob/main/Facial%20Verification%20with%20a%20Siamese%20Network%20-%20Final.ipynb
+# Training loop that goes through all the epochs set
 def train(siamese_model, dataset, checkpoint, checkpoint_prefix, binary_cross_loss, opt, EPOCHS):
-    # Loop through epochs
-    for epoch in range(1, EPOCHS+1):
+
+    # Loop through each epoch
+    for epoch in range(1, EPOCHS + 1):
+
+    	# Printing the current status 
         print('\n Epoch {}/{}'.format(epoch, EPOCHS))
         progbar = tf.keras.utils.Progbar(len(dataset))
         
-        # Creating a metric object 
+        # Creating metric objects
         r = Recall()
         p = Precision()
+        a = Accuracy()
         
-        # Loop through each batch
+        # Loop through each batch in the dataset 
         for idx, batch in enumerate(dataset):
-            # Run train step here
+
+            # Single training step
             loss = train_step(siamese_model, batch, binary_cross_loss, opt)
+
+            # Prediction with current batch 
             yhat = siamese_model.predict(batch[:2])
+
+            # Update the metrics accordingly 
             r.update_state(batch[2], yhat)
             p.update_state(batch[2], yhat) 
-            progbar.update(idx+1)
-        print(loss.numpy(), r.result().numpy(), p.result().numpy())
+            a.update_state(batch[2], yhat)
+
+            # Update the progress bar 
+            progbar.update(idx + 1)
+
+        # Printing the metrics values after completing a single epoch     
+        print(loss.numpy(), r.result().numpy(), p.result().numpy(), a.result().numpy())
         
-        # Save checkpoints
+        # Save weights each time 10 epochs have passed
         if epoch % 10 == 0: 
-            checkpoint.save(file_prefix=checkpoint_prefix)
+            checkpoint.save(file_prefix = checkpoint_prefix)
     
-    # Save weights
+    # Save weights after having completed training 
     siamese_model.save('VGG16.h5')
 
 
 # Builds the data and model to train it 
 def run_model(A1, B1, A0, B0):
 
-    # Settings for GPU, and a check
+    # Settings for GPU, and a check if the gpu was detected 
     phy_dev = tf.config.experimental.list_physical_devices('GPU')
     print("Num gpu: ", len(phy_dev))
     tf.config.experimental.set_memory_growth(phy_dev[0], True)
 
-    # Getting the dataset into Tensorflow format 
+    # Preprocessing the data before passing to model 
     dataset = build_data(A1, B1, A0, B0)
 
-    # Building the model with the dimensions of a patch
+    # Building the model with the dimensions according the dataset
     siamese_model = build_Siamese_network()
 
     # Summary of the model as confirmation
     siamese_model.summary()
 
     # Below comes from: https://github.com/nicknochnack/FaceRecognition/blob/main/Facial%20Verification%20with%20a%20Siamese%20Network%20-%20Final.ipynb
+
+    # Initializing the loss function
     binary_cross_loss = tf.losses.BinaryCrossentropy()
 
-    opt = tf.keras.optimizers.Adam(1e-4) # 0.0001
+    # Initializing the model's optimizer according to the value found in the paper 
+    opt = tf.keras.optimizers.Adam(0.001) 
 
     checkpoint_dir = './training_checkpoints'
     checkpoint_prefix = os.path.join(checkpoint_dir, 'ckpt')
-    checkpoint = tf.train.Checkpoint(opt=opt, siamese_model = siamese_model)
+    checkpoint = tf.train.Checkpoint(opt = opt, siamese_model = siamese_model)
 
     train(siamese_model, dataset, checkpoint, checkpoint_prefix, binary_cross_loss, opt, 10)
 
